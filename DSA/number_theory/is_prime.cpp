70c0ad443382fbf2986358bee7624d12fe844615
HarshNag1 <harshnag235@gmail.com>
2026-07-29 09:31:13 +0530

    Formatted:   DSA/number_theory/is_prime.cpp 	modified:   DSA/number_theory/is_prime.cpp

--- DSA/number_theory/is_prime.cpp
+++ DSA/number_theory/is_prime.cpp
@@ -1,3 +1,12 @@
 #include <bits/stdc++.h>
 using namespace std;
-int main(){ int n=29; if(n<2){ cout<<"false"; return 0; } for(int i=2;i*i<=n;++i) if(n%i==0){ cout<<"false"; return 0; } cout<<"true"; }
+int main(){
+    int n=29; if(n<2){
+        cout<<"false";
+        return 0; }
+for(int i=2;i*i<=n;++i)
+    if(n%i==0){
+        cout<<"false";
+        return 0;
+}
+cout<<"true"; }
