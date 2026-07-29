36fc85813fac593b202532ba73d0706799a62b30
HarshNag1 <harshnag235@gmail.com>
2026-07-29 10:12:56 +0530

    modified:   DSA/arrays/majority_element.cpp

--- DSA/arrays/majority_element.cpp
+++ DSA/arrays/majority_element.cpp
@@ -1,3 +1,11 @@
 #include <bits/stdc++.h>
 using namespace std;
-int main(){ vector<int>a={3,2,3}; int count=0,candidate=0; for(int x:a){ if(count==0) candidate=x; count += x==candidate?1:-1; } cout<<candidate; }
+int main() {
+  vector<int> a = {3, 2, 3};
+  int count = 0, candidate = 0;
+  for (int x : a) {
+    if (count == 0) candidate = x;
+    count += x == candidate ? 1 : -1;
+  }
+  cout << candidate;
+}
